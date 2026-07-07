20682bec5959f165eed9bb858375fbcc540858dc
Dsssyc <dsssyc02@gmail.com>
2026-07-07 11:42:47 +0800

    fix: restore dem underwater camera matrix

--- examples/m_demLayer/main.js
+++ examples/m_demLayer/main.js
@@ -8,25 +8,31 @@ if (!mapApi) {
     throw new Error('Map runtime failed to load for examples/m_demLayer')
 }
 
-const rasterStyle = {
+const underwaterTerrainMinElevation = -80.06899999999999 * 30.0
+
+const darkMatterStyle = {
     version: 8,
     sources: {
-        osm: {
+        cartoDarkMatter: {
             type: 'raster',
             tiles: [
-                'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                'https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
+                'https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
+                'https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
+                'https://d.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
             ],
             tileSize: 256,
-            attribution: 'OpenStreetMap contributors',
+            attribution: 'OpenStreetMap contributors, CARTO',
         },
     },
     layers: [
         {
-            id: 'osm',
+            id: 'carto-dark-matter',
             type: 'raster',
-            source: 'osm',
+            source: 'cartoDarkMatter',
+            paint: {
+                'raster-opacity': 0.92,
+            },
         },
     ],
 }
@@ -47,7 +53,7 @@ document.body.appendChild(mapDiv)
 scr.StartDash().then(() => {
 
     const map = new ScratchMap({
-        style: rasterStyle,
+        style: darkMatterStyle,
         center: [ 120.980697, 31.684162 ],
         projection: 'mercator',
         GPUFrame: GPUFrame,
@@ -134,6 +140,7 @@ class ScratchMap extends mapApi.Map {
         const cameraPosition = this.transform.getCameraPosition()
         this.mercatorCenter = mapApi.MercatorCoordinate.fromLngLat(cameraPosition.lngLat, cameraPosition.altitude)
         this.zoom.n = this.getZoom()
+        const { far, near, matrix } = getScratchMercatorMatrix(this.transform)
 
         const mercatorCenterX = encodeFloatToDouble(this.mercatorCenter.x)
         const mercatorCenterY = encodeFloatToDouble(this.mercatorCenter.y)
@@ -153,9 +160,9 @@ class ScratchMap extends mapApi.Map {
         this.mercatorBounds.reset(...m_sw, ...m_ne)
         this.cameraBounds.reset(...this.getBounds().toArray().flat())
 
-        this.far.n = this.transform.farZ
-        this.near.n = this.transform.nearZ
-        this.uMatrix.data = Float32Array.from(this.transform.mercatorMatrix)
+        this.far.n = far
+        this.near.n = near
+        this.uMatrix.data = matrix
         this.uMatrix.translate(scr.vec3f(mercatorCenterX[0], mercatorCenterY[0], mercatorCenterZ[0]))
     }
 
@@ -173,6 +180,119 @@ class ScratchMap extends mapApi.Map {
 }
 
 // Helpers //////////////////////////////////////////////////////////////////////////////////////////////////////
+function getScratchMercatorMatrix(transform) {
+
+    if (!transform.height || !transform.mercatorMatrix) {
+        return {
+            far: transform.farZ,
+            near: transform.nearZ,
+            matrix: Float32Array.from(transform.mercatorMatrix ?? scr.mat4.identity()),
+        }
+    }
+
+    const near = transform.height / 50
+    const far = calculateFarZForTerrainPlane(transform, underwaterTerrainMinElevation)
+    const offset = transform.centerOffset ?? { x: 0, y: 0 }
+    const point = transform.point
+    const fov = radiansFromTransformValue(transform._fov, transform.fov)
+    const pitch = radiansFromTransformValue(transform._pitch, transform.pitch)
+    const angle = radiansFromTransformValue(transform.angle, -transform.bearing)
+    const cameraToCenterDistance = getCameraToCenterDistance(transform, fov)
+
+    // Match MapLibre's Mercator custom-layer matrix, but lower the far-plane anchor for underwater DEM.
+    let matrix = scr.mat4.perspective(fov, transform.width / transform.height, near, far)
+    matrix[8] = -offset.x * 2 / transform.width
+    matrix[9] = offset.y * 2 / transform.height
+
+    scr.mat4.scale(matrix, [ 1, -1, 1 ], matrix)
+    scr.mat4.translate(matrix, [ 0, 0, -cameraToCenterDistance ], matrix)
+    scr.mat4.rotateX(matrix, pitch, matrix)
+    scr.mat4.rotateZ(matrix, angle, matrix)
+    scr.mat4.translate(matrix, [ -point.x, -point.y, 0 ], matrix)
+
+    matrix = scr.mat4.scale(matrix, [ transform.worldSize, transform.worldSize, transform.worldSize ])
+
+    return {
+        far,
+        near,
+        matrix: Float32Array.from(matrix),
+    }
+}
+
+function calculateFarZForTerrainPlane(transform, minElevation) {
+
+    const fov = radiansFromTransformValue(transform._fov, transform.fov)
+    const pitch = radiansFromTransformValue(transform._pitch, transform.pitch)
+    const offset = transform.centerOffset ?? { x: 0, y: 0 }
+    const pixelPerMeter = getPixelPerMeter(transform)
+    const elevation = getFiniteNumber(transform.elevation, transform._elevation, 0)
+    const currentTileMinElevation = getFiniteNumber(transform.minElevationForCurrentTile, elevation)
+    const visibleMinElevation = Math.min(elevation, currentTileMinElevation, minElevation)
+    const cameraToCenterDistance = getCameraToCenterDistance(transform, fov)
+
+    const cameraToSeaLevelDistance = cameraToCenterDistance + elevation * pixelPerMeter / Math.cos(pitch)
+    const cameraToLowestPointDistance = cameraToSeaLevelDistance - visibleMinElevation * pixelPerMeter / Math.cos(pitch)
+    const lowestPlane = visibleMinElevation < 0 ? cameraToLowestPointDistance : cameraToSeaLevelDistance
+    const groundAngle = Math.PI / 2 + pitch
+    const fovAboveCenter = fov * (0.5 + offset.y / transform.height)
+    const topHalfSurfaceDistance = Math.sin(fovAboveCenter) * lowestPlane / Math.sin(clamp(Math.PI - groundAngle - fovAboveCenter, 0.01, Math.PI - 0.01))
+    const horizon = typeof transform.getHorizon === 'function' ? transform.getHorizon() : Infinity
+    let topHalfSurfaceDistanceHorizon = Infinity
+
+    if (Number.isFinite(horizon) && horizon > 0) {
+        const horizonAngle = Math.atan(horizon / cameraToCenterDistance)
+        const fovCenterToHorizon = 2 * horizonAngle * (0.5 + offset.y / (horizon * 2))
+        topHalfSurfaceDistanceHorizon = Math.sin(fovCenterToHorizon) * lowestPlane / Math.sin(clamp(Math.PI - groundAngle - fovCenterToHorizon, 0.01, Math.PI - 0.01))
+    }
+
+    const topHalfMinDistance = Math.min(topHalfSurfaceDistance, topHalfSurfaceDistanceHorizon)
+    return (Math.cos(Math.PI / 2 - pitch) * topHalfMinDistance + lowestPlane) * 1.01
+}
+
+function getCameraToCenterDistance(transform, fov) {
+
+    return getFiniteNumber(
+        transform.cameraToCenterDistance,
+        0.5 / Math.tan(fov / 2) * transform.height
+    )
+}
+
+function getPixelPerMeter(transform) {
+
+    return getFiniteNumber(
+        transform._pixelPerMeter,
+        transform.pixelsPerMeter,
+        mercatorZfromAltitude(1, transform.center.lat) * transform.worldSize
+    )
+}
+
+function radiansFromTransformValue(privateRadians, publicDegrees) {
+
+    return Number.isFinite(privateRadians) ? privateRadians : publicDegrees * Math.PI / 180
+}
+
+function getFiniteNumber(...values) {
+
+    return values.find((value) => Number.isFinite(value))
+}
+
+function clamp(value, min, max) {
+
+    return Math.min(Math.max(value, min), max)
+}
+
+function circumferenceAtLatitude(latitude) {
+
+    const earthRadius = 6371008.8
+    const earthCircumference = 2 * Math.PI * earthRadius
+    return earthCircumference * Math.cos(latitude * Math.PI / 180)
+}
+
+function mercatorZfromAltitude(altitude, latitude) {
+
+    return altitude / circumferenceAtLatitude(latitude)
+}
+
 function encodeFloatToDouble(value) {
     const result = new Float32Array(2);
     result[0] = value;
